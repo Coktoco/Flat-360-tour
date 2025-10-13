5ba624b2a297c7f325c2a30794bc1cba10f31b93
Coktoco <albert.winiarski6@gmail.com>
2025-10-13 23:47:59 +0200

    Commit entire project

---
+++ data.js
@@ -0,0 +1,263 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_5971",
+      "name": "IMG_5971",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2091745675991277,
+          "pitch": 0.48292605447576165,
+          "rotation": 0,
+          "target": "2-korytarz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-salon-360_pelny_z_grzejnikami",
+      "name": "Salon-360_pelny_z_grzejnikami",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8974231219289042,
+          "pitch": 0.5715940825921884,
+          "rotation": 6.283185307179586,
+          "target": "2-korytarz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-korytarz",
+      "name": "Korytarz",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.9286092145351414,
+        "pitch": 0.26335351960774567,
+        "fov": 1.564601509346989
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.300324095240999,
+          "pitch": 0.67310270647992,
+          "rotation": 0,
+          "target": "1-salon-360_pelny_z_grzejnikami"
+        },
+        {
+          "yaw": 0.24065262688542077,
+          "pitch": 0.6002525310408799,
+          "rotation": 0,
+          "target": "4-lazienka_lepsza"
+        },
+        {
+          "yaw": 1.7281810173671976,
+          "pitch": 0.5648536291733812,
+          "rotation": 0,
+          "target": "3-lazienka_dodatkowa_lepsza"
+        },
+        {
+          "yaw": 1.0819480329417779,
+          "pitch": 0.42759968753538224,
+          "rotation": 0,
+          "target": "0-img_5971"
+        },
+        {
+          "yaw": 1.4610091121575106,
+          "pitch": 0.41010471150840466,
+          "rotation": 0,
+          "target": "5-sypialnia_lepsza"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-lazienka_dodatkowa_lepsza",
+      "name": "Lazienka_dodatkowa_lepsza",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3663455456534823,
+          "pitch": 0.6708385991574382,
+          "rotation": 0,
+          "target": "2-korytarz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-lazienka_lepsza",
+      "name": "Lazienka_lepsza",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1142446849515473,
+          "pitch": 0.5657554069712472,
+          "rotation": 0,
+          "target": "2-korytarz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sypialnia_lepsza",
+      "name": "Sypialnia_lepsza",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.343150251036052,
+          "pitch": 0.42943372209053976,
+          "rotation": 0,
+          "target": "2-korytarz"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
